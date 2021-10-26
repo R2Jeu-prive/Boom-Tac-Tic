7038e99019b57600c305c080e973068d8d9a917e
PierreGueguen <>
2021-10-26 12:53:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname })
+});
+
+app.get('/script', (req, res) => {
+  res.sendFile("src/script.js", {root: __dirname })
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
